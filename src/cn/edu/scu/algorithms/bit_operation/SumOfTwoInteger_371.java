762946791f9ceecd4cfd4f2445cd4ba0f5265935
cht <916446837@qq.com>
2020-03-31 12:31:30 +0800

    提交lc374
    Signed-off-by: cht <916446837@qq.com>

---
+++ src/cn/edu/scu/algorithms/bit_operation/SumOfTwoInteger_371.java
@@ -0,0 +1,27 @@
+package cn.edu.scu.algorithms.bit_operation;
+
+/**
+ * 371. 两整数之和
+ * 不使用运算符 + 和 - ​​​​​​​，计算两整数 ​​​​​​​a 、b ​​​​​​​之和。
+ *
+ * 示例 1:
+ *
+ * 输入: a = 1, b = 2
+ * 输出: 3
+ * 示例 2:
+ *
+ * 输入: a = -2, b = 3
+ * 输出: 1
+ */
+public class SumOfTwoInteger_371 {
+
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int sum = a^b;
+            int carry = (a&b) << 1;
+            a = sum;
+            b = carry;
+        }
+        return a;
+    }
+}
