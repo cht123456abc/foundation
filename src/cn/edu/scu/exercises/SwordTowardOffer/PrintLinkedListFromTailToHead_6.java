0c889c2806280fd60abc9b83685c17c80deeacfe
cht <916446837@qq.com>
2020-04-13 23:44:13 +0800

    提交剑指offer 6
    Signed-off-by: cht <916446837@qq.com>

---
+++ src/cn/edu/scu/exercises/SwordTowardOffer/PrintLinkedListFromTailToHead_6.java
@@ -0,0 +1,31 @@
+package cn.edu.scu.exercises.SwordTowardOffer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 面试题06. 从尾到头打印链表
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：head = [1,3,2]
+ * 输出：[2,3,1]
+ */
+public class PrintLinkedListFromTailToHead_6 {
+
+    public int[] reversePrint(ListNode head) {
+        List<Integer> res = new ArrayList<>();
+        while(head != null){
+            res.add(head.val);
+            head = head.next;
+        }
+        Collections.reverse(res);
+        int[] r = res.stream().mapToInt(Integer::intValue).toArray();
+        return r;
+    }
+}
