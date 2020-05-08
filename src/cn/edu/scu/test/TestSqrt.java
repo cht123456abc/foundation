7245dc8345f7927b24bb324180122248e07de5bc
cht <916446837@qq.com>
2020-05-08 19:48:49 +0800

    leetcode 221
    Signed-off-by: cht <916446837@qq.com>

---
+++ src/cn/edu/scu/test/TestSqrt.java
@@ -0,0 +1,11 @@
+package cn.edu.scu.test;
+
+public class TestSqrt {
+
+    public static void main(String[] args) {
+
+
+        int a = 17;
+        System.out.println((int)Math.sqrt(a));
+    }
+}
