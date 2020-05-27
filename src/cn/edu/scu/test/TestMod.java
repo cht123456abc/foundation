45022f94a367395521282697ae4e1dc409339aeb
cht <916446837@qq.com>
2020-05-27 15:06:57 +0800

    leetcode 974 前缀和 同余定理
    Signed-off-by: cht <916446837@qq.com>

---
+++ src/cn/edu/scu/test/TestMod.java
@@ -0,0 +1,12 @@
+package cn.edu.scu.test;
+
+public class TestMod {
+
+    public static void main(String[] args) {
+
+        int a = -1;
+        int b = a % 3;
+        System.out.println(b);
+        System.out.println((b + 3) % 3);
+    }
+}
