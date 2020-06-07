3c4322b5de41911f27a837c824970e77916007f9
cht <916446837@qq.com>
2020-06-07 11:32:37 +0800

    leetcode 974 前缀和 同余定理
    Signed-off-by: cht <916446837@qq.com>

---
+++ src/cn/edu/scu/test/TestCharacter.java
@@ -0,0 +1,12 @@
+package cn.edu.scu.test;
+
+public class TestCharacter {
+
+    public static void main(String[] args) {
+
+        Character c = 'a';
+        StringBuilder sb = new StringBuilder();
+        sb.reverse();
+        String a = "aa";
+    }
+}
