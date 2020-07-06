9be90adb27646e502ce3074d195bfbe582898c15
cht <916446837@qq.com>
2020-07-06 18:22:42 +0800

    全排列
    Signed-off-by: cht <916446837@qq.com>

--- src/cn/edu/scu/test/Test.java
+++ src/cn/edu/scu/test/Test.java
@@ -2,12 +2,20 @@ package cn.edu.scu.test;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Test {
 
 
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
-        Set<String> set = list.stream().collect(Collectors.toSet());
+        List<Character> a = new ArrayList<>();
+
+        Character[] chars = a.toArray(new Character[0]);
+        StringBuilder sb = new StringBuilder();
+        sb.append("123");
+        sb.delete(0,1);
+        System.out.println(sb.toString());
+
     }
 }
