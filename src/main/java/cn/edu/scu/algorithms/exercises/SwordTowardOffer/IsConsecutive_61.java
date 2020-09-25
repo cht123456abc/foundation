064ec39a39e76456440925a32e1fdfd33632baa4
chenhengtai <916446837@qq.com>
2020-09-25 09:34:45 +0800

    提交

---
+++ src/main/java/cn/edu/scu/algorithms/exercises/SwordTowardOffer/IsConsecutive_61.java
@@ -0,0 +1,47 @@
+package cn.edu.scu.algorithms.exercises.SwordTowardOffer;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+/**
+ * 剑指 Offer 61. 扑克牌中的顺子
+ * 从扑克牌中随机抽5张牌，判断是不是一个顺子，即这5张牌是不是连续的。2～10为数字本身，A为1，J为11，Q为12，K为13，而大、小王为 0 ，可以看成任意数字。A 不能视为 14。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3,4,5]
+ * 输出: True
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: [0,0,1,2,5]
+ * 输出: True
+ *
+ *
+ * 限制：
+ *
+ * 数组长度为 5
+ *
+ * 数组的数取值为 [0, 13] .
+ */
+public class IsConsecutive_61 {
+
+
+    public boolean isStraight(int[] nums) {
+        int max = Arrays.stream(nums).max().getAsInt();
+        int min = Arrays.stream(nums).filter(a -> a > 0).min().getAsInt();
+        boolean dup = Arrays.stream(nums).filter(a -> a > 0).boxed().collect(Collectors.toSet()).size() != Arrays.stream(nums).filter(a -> a > 0).count();
+
+
+        return !dup && max - min <= 4;
+
+
+
+
+
+    }
+}
--- src/test/java/TestInput.java
+++ src/test/java/TestInput.java
@@ -1,24 +1,22 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class TestInput {
 
     public static void main(String[] args) {
-//        Scanner input = new Scanner(System.in);
-//        String s = input.nextLine();
-//        input.close();
+        String content = "SELECT pr_rule.*,sys_task_rule.`rule_type`,sys_task_rule.`interval_time`,sys_task_rule.`cron_exp` FROM (\n" +
+                "SELECT pre_pr.*,parse_rule.`rule_name`,parse_rule.`dependency_depth`,parse_rule.`default_rule` FROM (\n" +
+                "SELECT sub_1.*,COUNT(CASE WHEN vul_level=\"4\" THEN vul_level END) level4,COUNT(CASE WHEN vul_level=\"3\" THEN vul_level END) level3,COUNT(CASE WHEN vul_level=\"2\" THEN vul_level END) level2,COUNT(CASE WHEN vul_level=\"1\" THEN vul_level END) level1,COUNT(CASE WHEN vul_level=\"0\" THEN vul_level END) level0 FROM (\n" +
+                "SELECT sub.*,count(repo_items.repo_name) AS lib_number,count(DISTINCT repo_items.license) AS license_types FROM (\n" +
+                "SELECT UUID() AS id,prod.NAME AS product_name,prod.id AS product_id,ptask.*FROM (\n" +
+                "SELECT id,NAME FROM product WHERE user_id=189) prod LEFT JOIN (\n" +
+                "SELECT t.*,p.`product_id` AS pd_id,p.NAME,p.`create_time`,p.`last_scan`,p.`url`,p.`project_source`,p.`rule_id` FROM project p,(\n" +
+                "SELECT id AS tid,project_id,msg,STATUS,`progress` FROM sys_task WHERE id IN (\n" +
+                "SELECT MAX(id) AS id FROM sys_task GROUP BY project_id)) t WHERE t.project_id=p.id) ptask ON prod.id=ptask.pd_id) sub LEFT JOIN repo_items ON sub.project_id=repo_items.project_id GROUP BY sub.project_id) sub_1 LEFT JOIN defects USING (project_id) GROUP BY sub_1.project_id) pre_pr LEFT JOIN parse_rule ON pre_pr.rule_id=parse_rule.id) pr_rule LEFT JOIN sys_task_rule ON pr_rule.project_id=sys_task_rule.project_id WHERE (\n" +
+                "SELECT IF (LENGTH(TRIM(\"\"))> 0,pr_rule.NAME LIKE '%' \"\" '%',1=1)) ORDER BY create_time DESC";
 
-        System.out.println("===============");
-
-        BufferedReader input2 = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            String s2 = input2.readLine();
-            input2.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("++++++++++");
+        System.out.println(content.replaceAll("\n",""));
     }
 }
