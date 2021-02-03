569c9d88165d0be0b42d0398fdd0b9344923eef3
cht <916446837@qq.com>
2021-02-03 14:42:27 +0800

    收集dp题

---
+++ src/main/java/cn/edu/scu/algorithms/dp/CutRopeI_14.java
@@ -0,0 +1,48 @@
+package cn.edu.scu.algorithms.dp;
+
+/**
+ *面试题14- I. 剪绳子
+ * 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+ *
+ * 示例 1：
+ *
+ * 输入: 2
+ * 输出: 1
+ * 解释: 2 = 1 + 1, 1 × 1 = 1
+ * 示例 2:
+ *
+ * 输入: 10
+ * 输出: 36
+ * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36
+ * 提示：
+ *
+ * 2 <= n <= 58
+ */
+public class CutRopeI_14 {
+
+    // public int cuttingRope(int n) {
+    //     // 数学推导：当切分段的长度为3，且长度为3的段数尽可能多的时候取得乘积得最大值
+    //     if(n <= 3) return n-1;
+    //     int a = n / 3,b = n % 3;
+    //     // 根据最后一段b得长度分三种情况讨论
+    //     if(b == 0) return (int)Math.pow(3,a);
+    //     if(b == 1) return (int)Math.pow(3,a-1) * 4;
+    //     if(b == 2) return (int)Math.pow(3,a) * 2;
+    //     return -1;
+
+    // }
+
+    // 动态规划解
+    public int cuttingRope(int n) {
+        if(n <= 3) return n-1;
+
+        // 当n > 3的时候，最后一段长度只能是2或者3
+        int[] dp = new int[n+1];
+        dp[1] = 1;dp[2] = 2;dp[3] = 3;
+        for(int i = 4;i<=n;i++){
+            dp[i] = Math.max(dp[i-2]*2,dp[i-3]*3);
+        }
+        return dp[n];
+
+    }
+}
---
+++ src/main/java/cn/edu/scu/algorithms/dp/OneEditDistance_0105.java
@@ -0,0 +1,87 @@
+package cn.edu.scu.algorithms.dp;
+
+
+/**
+ *面试题 01.05. 一次编辑
+ * 字符串有三种编辑操作:插入一个字符、删除一个字符或者替换一个字符。 给定两个字符串，编写一个函数判定它们是否只需要一次(或者零次)编辑。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * first = "pale"
+ * second = "ple"
+ * 输出: True
+ *
+ *
+ * 示例 2:
+ *
+ * 输入:
+ * first = "pales"
+ * second = "pal"
+ * 输出: False
+ */
+public class OneEditDistance_0105 {
+
+    // 双指针
+    public boolean oneEditAway(String first, String second) {
+
+        int lenA = first.length();
+        int lenB = second.length();
+        // 保证第二个字符串更长
+        if (lenA > lenB) return oneEditAway(second, first);
+        if (lenB - lenA > 1) return false;
+
+        boolean flag = false;
+        for (int i = 0, j = 0; i < lenA && j < lenB; i++, j++) {
+            if (first.charAt(i) != second.charAt(j)) {
+                // 只允许1次不相等
+                if (flag) return false;
+                flag = true;
+                // 继续比较后续子串
+                if (lenA != lenB) i--;// 第一个字符串不变
+            }
+        }
+        return true;
+    }
+
+
+    // 动态规划 最小编辑距离
+    public boolean oneEditAway1(String first, String second) {
+        // dp[i][j] : 匹配字符串1中i位置时，字符串2j位置时需要编辑的次数
+
+        int lenA = first.length();
+        int lenB = second.length();
+        // 剪枝
+        if (Math.abs(lenA - lenB) > 1) return false;
+
+        // 确定状态表达式
+        int[][] dp = new int[lenA + 1][lenB + 1];
+
+        // 边界条件
+        // 当第一个字符串为”“时
+        for (int j = 1; j <= lenB; j++) {
+            dp[0][j] = j;
+        }
+        // 当第二个字符串为”“时
+        for (int i = 1; i <= lenA; i++) {
+            dp[i][0] = i;
+        }
+
+        // 递推顺序和状态转移方程
+        for (int i = 1; i <= lenA; i++) {
+            for (int j = 1; j <= lenB; j++) {
+                if (first.charAt(i - 1) == second.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    // 这个时候有三种情况：删除、增加、替换（两个字符串长度相同的时候）
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1] + 1);
+                }
+            }
+        }
+
+        return dp[lenA][lenB] <= 1;
+    }
+
+}
