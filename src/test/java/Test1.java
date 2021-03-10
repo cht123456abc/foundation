2f9b94a32161e9d1b377b8feaf57a5b86f41063d
cht <916446837@qq.com>
2021-03-10 22:13:20 +0800

    环入口检测

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Test1 {
 
 
     public static void main(String[] args) {
-
+        List<Integer> list = new ArrayList<>();
+        int[] a = list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
