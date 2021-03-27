8ff56c95a7563ebbb834c5d49016831cdaad19e8
cht <916446837@qq.com>
2021-03-27 22:24:37 +0800

    接雨水

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -7,8 +7,8 @@ public class Test1 {
 
     public static void main(String[] args) {
 
-        int a = 170 + (3*91) + (270) + 160 + 87 + 172 + 78 + 4*85;
-        int b = 18;
-        System.out.println((float) a / b);
+        List<Integer> res = new ArrayList<>();
+
+        res.stream().mapToInt(Integer::intValue).toArray();
     }
 }
