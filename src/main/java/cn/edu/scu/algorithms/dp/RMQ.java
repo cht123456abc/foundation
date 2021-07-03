cb26bfcc0a7e8e322c18e9446cb6679c204147d3
cht <916446837@qq.com>
2021-07-03 19:17:21 +0800

    RMQ问题 st表

--- src/main/java/cn/edu/scu/algorithms/dp/RMQ.java
+++ src/main/java/cn/edu/scu/algorithms/dp/RMQ.java
@@ -1,38 +1,80 @@
 package cn.edu.scu.algorithms.dp;
-
+import java.util.Random;
 
 /**
- * 区间最值问题--st表
+ * 区间最值问题
  */
 public class RMQ {
 
-    int n = 5;
-    int[][] dp = new int[n][25];
-    int[] a = new int[]{1, 2, 3, 4, 5};
+    static int M;
 
-    private void init() {
+    public static void main(String[] args) {
+        Random random = new Random();
+        final int count = (int)1e7;
+        int[] a = new int[1000];
+        for (int i = 0; i < 1000; i++) {
+            a[i] = random.nextInt(1000);
+        }
 
+        // st表
+        ST st = new ST(a);
+        st.init();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            int l = random.nextInt(500);
+            int r = 500 + random.nextInt(500);
+            M = st.query(l, r);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("st表耗时：" + (end - start) + "ms");
+
+        // 暴力
+        start = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            int l = random.nextInt(500);
+            int r = 500 + random.nextInt(500);
+            int min = Integer.MAX_VALUE;
+            for (int j = l; j <= r; j++) {
+                if (a[j] < min) min = a[j];
+            }
+            M = min;
+        }
+        end = System.currentTimeMillis();
+        System.out.println("暴力法耗时：" + (end - start) + "ms");
+
+    }
+}
+
+
+/**
+ * ST表
+ */
+class ST {
+    int n;
+    int[] a;
+    int[][] dp;// st表,dp[i][j] 代表从a[i]开始，到a[i+2^j-1]即区间[a[i],a[i+2^j-1]]的最值
+
+    public ST(int[] a) {
+        this.a = a;
+        this.n = a.length;
+        this.dp = new int[n][31];
+    }
+
+    // 初始化 T=O(n)
+    public void init() {
         for (int i = 0; i < n; i++) {
             dp[i][0] = a[i];
         }
         for (int j = 1; (1 << j) <= n; j++) {
             for (int i = 0; i + (1 << j) - 1 < n; i++) {
-                dp[i][j] = Math.min(dp[i][j-1],dp[i+(1<<j-1)][j-1]);
+                dp[i][j] = Math.min(dp[i][j-1],dp[i+(1<<(j-1))][j-1]);
             }
         }
     }
 
-    private int query(int l, int r) {
+    // 查询区间最值 T=O(1)
+    public int query(int l, int r) {
         int k = (int)Math.log(r - l + 1);
         return Math.min(dp[l][k], dp[r - (1 << k)+1][k]);
-    }
-
-    public static void main(String[] args) {
-        RMQ rmq = new RMQ();
-        rmq.init();
-        System.out.println(rmq.query(1, 2));
-        System.out.println(rmq.query(3, 4));
-        System.out.println(rmq.query(2, 3));
-
     }
 }
---
+++ src/main/java/cn/edu/scu/algorithms/st/P3865.java
@@ -0,0 +1,52 @@
+package cn.edu.scu.algorithms.st;
+
+
+import java.util.Scanner;
+
+public class P3865 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        ST st = new ST(a);
+        st.init();
+        for (int i = 0; i < m; i++) {
+            int l = scanner.nextInt();
+            int r = scanner.nextInt();
+            System.out.println(st.query(l-1,r-1));
+        }
+    }
+
+}
+
+class ST {
+    int[] a;
+    int[][] dp;
+
+    public ST(int[] a) {
+        this.a = a;
+        this.dp = new int[a.length][31];
+    }
+
+    public void init() {
+        for (int i = 0; i < a.length; i++) {
+            dp[i][0] = a[i];
+        }
+        for (int j = 1; (1 << j) <= a.length; j++) {
+            for (int i = 0; i + (1 << j) - 1 < a.length; i++) {
+                dp[i][j] = Math.max(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
+            }
+        }
+    }
+
+    public int query(int l, int r) {
+        int k = (int)(Math.log(r-l+1) / Math.log(2));
+        return Math.max(dp[l][k], dp[r - (1 << k) + 1][k]);
+    }
+}
