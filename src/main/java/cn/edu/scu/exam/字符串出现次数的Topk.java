c33d631757b6b30adb54399c4977bb38d8ff15f0
cht <916446837@qq.com>
2021-09-15 16:23:06 +0800

    完全平方数和

--- src/main/java/cn/edu/scu/exam/字符串出现次数的Topk.java
+++ src/main/java/cn/edu/scu/exam/字符串出现次数的Topk.java
@@ -15,7 +15,7 @@ public class 字符串出现次数的Topk {
             int count1 = map.get(o1);
             int count2 = map.get(o2);
             if (count1 == count2) {
-                return compareStrings(o1, o2);
+                return o1.compareTo(o2);// 字符串的字典序排序
             }
             return count2 - count1;
         });
@@ -33,6 +33,7 @@ public class 字符串出现次数的Topk {
         return ans;
     }
 
+    // 自写字典序排序
     public int compareStrings(String s1, String s2) {
         int n1 = s1.length();
         int n2 = s2.length();
@@ -41,8 +42,9 @@ public class 字符串出现次数的Topk {
         for (int i = 0; i < n1 || i < n2; i++) {
             if (i >= n1) return -1;
             if (i >= n2) return 1;
-            if (ss1[i] < ss2[i]) return -1;
-            if (ss1[i] > ss2[i]) return 1;
+            if(ss1[i] != ss2[i]){
+                return ss1[i] - ss2[i];
+            }
         }
         return 0;
     }
