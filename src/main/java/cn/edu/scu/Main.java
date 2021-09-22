b39fceb797e161b97bf068f8605897742f01df05
CHT <916446837@qq.com>
2021-09-22 20:21:37 +0800

    1

--- src/main/java/cn/edu/scu/Main.java
+++ src/main/java/cn/edu/scu/Main.java
@@ -1,25 +1,49 @@
 package cn.edu.scu;
 
-import java.io.Externalizable;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
 
-        int[][] mat = new int[n][n];
-        long res = 0;
+        int k = scanner.nextInt();
+
+        int[] mat = new int[n];
+
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                mat[i][j] = scanner.nextInt();
-                res += mat[i][j];
-            }
+            mat[i] = scanner.nextInt();
+
         }
 
+        if (mat[k - 1] == 0) {
+
+            System.out.println(0);
+            return;
+        }
+
+        int mid = k - 1;
+        long res = 0;
+
+        int j = mid + 1;
+        while (j < n && mat[j] > 1) {
+            res += mat[j];
+            j++;
+        }
+        if (j < n && mat[j] == 1) res++;
+
+        j = mid - 1;
+        while (j >= 0 && mat[j] > 1) {
+            res += mat[j];
+            j--;
+        }
+        if (j >= 0 && mat[j] == 1) res++;
+
+        res += mat[mid];
         System.out.println(res);
 
     }
