14693492b398e654a4f90560eceb60d066f833cf
chenhengtai <916446837@qq.com>
2023-02-22 00:09:14 +0800

    和为target的连续子数组

--- src/main/java/cn/edu/scu/Main.java
+++ src/main/java/cn/edu/scu/Main.java
@@ -6,11 +6,6 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
 
-        int[] a = new int[]{2, 1, 6};
-
-        Arrays.sort(a);
-
-        Arrays.stream(a).forEach(System.out::println);
 
     }
 }
--- src/main/java/cn/edu/scu/algorithms/exercises/sword_toward_offer/ContinuousSeqSum_57_II.java
+++ src/main/java/cn/edu/scu/algorithms/exercises/sword_toward_offer/ContinuousSeqSum_57_II.java
@@ -28,6 +28,33 @@ import java.util.List;
  */
 public class ContinuousSeqSum_57_II {
 
+
+
+    // 双指针
+    public int[][] findContinuousSequence(int target) {
+
+        int l = 1,r = 2;
+        List<int[]> res = new ArrayList<>();
+        while(l<r){
+            int sum = (l+r) * (r-l+1) / 2;
+            if(sum == target){
+                List<Integer> one = new ArrayList<>();
+                for(int i = l;i<=r;i++){
+                    one.add(i);
+                }
+                res.add(one.stream().mapToInt(Integer::intValue).toArray());
+                l++;
+            } else if (sum > target) {
+                l++;
+            } else {
+                r++;
+            }
+        }
+        return res.toArray(new int[0][]);
+
+    }
+
+
     //     // 前缀和
     // public int[][] findContinuousSequence(int target) {
     //     int sum = 0;
@@ -70,25 +97,31 @@ public class ContinuousSeqSum_57_II {
     //     return res.toArray(new int[0][]);
     // }
 
-    // 大哥算法：15(target) = 7 + (7+1) = 4 + (4+1) + (4+2) =  x + (x+1) + ... + (x+i-1) = i*x + 1+...+i-1;
-    public int[][] findContinuousSequence(int target) {
-        List<int[]> result = new ArrayList<>();
-        int i = 1;
-        while(target>0)
-        {
-            target -= i++;
-            if(target>0 && target%i == 0)// 基数x = target / i 一定为整数
-            {
-                int[] array = new int[i];
-                for(int k = target/i, j = 0; k < target/i+i; k++,j++)
-                {
-                    array[j] = k;
-                }
-                result.add(array);
-            }
-        }
-        Collections.reverse(result);
-        return result.toArray(new int[0][]);
+//    // 大哥算法：15(target) = 7 + (7+1) = 4 + (4+1) + (4+2) =  x + (x+1) + ... + (x+i-1) = i*x + 1+...+i-1;
+//    public int[][] findContinuousSequence(int target) {
+//        List<int[]> result = new ArrayList<>();
+//        int i = 1;
+//        while(target>0)
+//        {
+//            target -= i++;
+//            if(target>0 && target%i == 0)// 基数x = target / i 一定为整数
+//            {
+//                int[] array = new int[i];
+//                for(int k = target/i, j = 0; k < target/i+i; k++,j++)
+//                {
+//                    array[j] = k;
+//                }
+//                result.add(array);
+//            }
+//        }
+//        Collections.reverse(result);
+//        return result.toArray(new int[0][]);
+//    }
+
+    public static void main(String[] args) {
+
+        ContinuousSeqSum_57_II continuousSeqSum_57_ii = new ContinuousSeqSum_57_II();
+        int[][] res = continuousSeqSum_57_ii.findContinuousSequence(9);
     }
 }
 
